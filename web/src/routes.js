ebd214c16862592222cb3858ed3dd09d8e1ce19a
Lucas Edilson <lucas@proesc.com>
2021-07-01 16:50:02 -0300

    Adicionada nova rota

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
+import Resultados from './Pages/Resultados'
 
 function Routes () {
     return (
         <BrowserRouter>
         <Route path="/" exact component={Home}/>
         <Route path="/dashboard" component={Dashboard}/>
+        <Route path="/resultados" component={Resultados}/>
         </BrowserRouter>
     )
 }
