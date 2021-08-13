d0c955c7e6c94343284dbbc702e6c1f5c69e5a48
Luma Montes <lumagoesmontes@gmail.com>
2021-08-13 20:37:37 -0300

    chamado funçao

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -88,6 +88,7 @@ export default function Home() {
         searchAureaneTickets();
         searchAlexandreTickets();
         searchDrianneTickets();
+        searchLucasPending();
 
     },[data])
 
