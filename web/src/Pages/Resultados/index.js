cc525aba3fab781825ec03ff16a121a7888940fc
Luma Montes <lumagoesmontes@gmail.com>
2021-08-31 19:32:33 -0300

    adicionado consultas de chat em index

--- web/src/Pages/Resultados/index.js
+++ web/src/Pages/Resultados/index.js
@@ -37,27 +37,43 @@ export default function Results() {
                 },
                 {
                     name:'uber',
-                    image:'https://trello-attachments.s3.amazonaws.com/603481895db937125c03e74e/6035669cd828620144745941/af370852f7956cc1da2c024e1e55ffd3/image.png'
+                    image:'https://trello-attachments.s3.amazonaws.com/603481895db937125c03e74e/6035669cd828620144745941/af370852f7956cc1da2c024e1e55ffd3/image.png',
+                    chat: false
                 },
                 {
                     name:'alexandre',
-                    image:'https://trello-attachments.s3.amazonaws.com/604bccc4d51c2b27a69d9cd4/195x197/1af6bb25c8f05b934a163386daabac39/Screenshot_65.png'
+                    image:'https://trello-attachments.s3.amazonaws.com/604bccc4d51c2b27a69d9cd4/195x197/1af6bb25c8f05b934a163386daabac39/Screenshot_65.png',
+                    chat: true
                 },
                 {
                     name:'gabriel',
-                    image:'https://ca.slack-edge.com/T56FFG3EW-U01PVV09MT6-f77d984f9feb-512'
+                    image:'https://ca.slack-edge.com/T56FFG3EW-U01PVV09MT6-f77d984f9feb-512',
+                    chat: true
                 },
                 {
                     name:'drianne',
-                    image:'https://trello-attachments.s3.amazonaws.com/6036b82d00e3de2b27796ff7/960x1280/3d94b336907be4f26cf5e2dd1ef60791/foto_atualizada.jpeg.jpg'
+                    image:'https://trello-attachments.s3.amazonaws.com/6036b82d00e3de2b27796ff7/960x1280/3d94b336907be4f26cf5e2dd1ef60791/foto_atualizada.jpeg.jpg',
+                    chat: true
                 },
                 {
                     name:'fernanda',
-                    image:'https://trello-attachments.s3.amazonaws.com/6035439118587528d04f1cf3/538x678/b8b4f30540823c59704b5976cd3ede74/image.png'
+                    image:'https://trello-attachments.s3.amazonaws.com/6035439118587528d04f1cf3/538x678/b8b4f30540823c59704b5976cd3ede74/image.png',
+                    chat: true
                 },
                 {
                     name:'giovanna',
-                    image:'https://trello-attachments.s3.amazonaws.com/60353fe15584ee18e0dd08d1/1153x2048/526869e1b4204291de6fe2d3fa9e6c54/EqgilqwXAAMboqL_-_Giovanna_Lourinho.png'
+                    image:'https://trello-attachments.s3.amazonaws.com/60353fe15584ee18e0dd08d1/1153x2048/526869e1b4204291de6fe2d3fa9e6c54/EqgilqwXAAMboqL_-_Giovanna_Lourinho.png',
+                    chat: true
+                },
+                {
+                    name:'duarte',
+                    image:'https://ca.slack-edge.com/T029TQP6WQ7-U02AY6116QG-30a8df6ae2c3-512',
+                    chat: true
+                },
+                {
+                    name:'aline',
+                    image:'https://ca.slack-edge.com/T02AY5X4DFS-U02A8N19NFM-b3aca9920403-512',
+                    chat: true
                 },
             ]
         );
@@ -72,7 +88,8 @@ export default function Results() {
                         infoAgent = {
                             ...infoAgent,
                             name:agente.name?agente.name:'Nao encontrado',
-                            image:agente.image?agente.image:'Nao encontrado'
+                            image:agente.image?agente.image:'Nao encontrado',
+                            chat: agente.chat
                         }
                         const response = await api.get(`/tickets/?filter=tags:${agente.name} status:solved status:closed solved>=${inicialDate} solved<=${finalDate}`);
                         infoAgent = {
@@ -96,6 +113,28 @@ export default function Results() {
                         infoAgent = {
                             ...infoAgent,
                             problem:response4.data?response4.data:0
+                        }
+                        if(agente.chat === true){
+                            const responseChat = await api.get(`/chats/?filter=agent_names:${agente.name} AND timestamp:[${inicialDate} TO ${finalDate}]`);
+                            infoAgent = {
+                                ...infoAgent,
+                                chats:responseChat.data? responseChat.data:0
+                            }
+
+                            const responseChatRating = await api.get(`/chats/?filter=agent_names:${agente.name} AND timestamp:[${inicialDate} TO ${finalDate}] AND rating:bad`);
+                            infoAgent = {
+                                ...infoAgent,
+                                chatRating:responseChatRating.data? responseChatRating.data:0
+                            }
+                        }else{
+                            infoAgent = {
+                                ...infoAgent,
+                                chats:0
+                            }
+                            infoAgent = {
+                                ...infoAgent,
+                                chatRating:0
+                            }
                         }
                         AgentsInfoTemp = [
                             ...AgentsInfoTemp,
@@ -140,7 +179,10 @@ export default function Results() {
                     return(
                             <AgentCard 
                                 agent_picture={agente.image}
-                                agent={agente.name} total={agente.total} question={agente.question} task={agente.task} problem={agente.problem}
+                                agent={agente.name} 
+                                chat={agente.chat}
+                                total={agente.total} question={agente.question} task={agente.task} problem={agente.problem}
+                                count_chats={agente.chats} satisfaction={agente.chats > 0 ? (100-((agente.chatRating) * 100/ agente.chats)).toFixed(2) : 0 } 
                             />   
                         
                     )
@@ -152,7 +194,7 @@ export default function Results() {
                 < IndicatorCard color="sucess" number={solvedTickets} text="Resolvidos"/>
                 < IndicatorCard color="info" number={pendingTickets} text="Pendentes"/>
                 < IndicatorCard color="warning" number={openTickets} text="Em aberto"/>
-            </div>
+            </div> 
         </div>
     )
 }
