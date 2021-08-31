b55def1556628d4a4f0e97aee125f6aa454eb203
Luma Montes <lumagoesmontes@gmail.com>
2021-08-31 20:34:02 -0300

    adicionado console

--- src/controllers/ChatsController.js
+++ src/controllers/ChatsController.js
@@ -3,12 +3,12 @@ const chatApi = require('../services/chatApi');
 module.exports = {
   async index(request, response) {
     const {filter} = request.query;
-    console.log(filter);
+    // console.log(filter);
 
     const chats = await chatApi.get(`/chats/search?q=${filter}`);
+    console.log(chats.data);
   
     const filteredChats = chats.data.count;
-  
     return response.json(filteredChats);
   },
 
